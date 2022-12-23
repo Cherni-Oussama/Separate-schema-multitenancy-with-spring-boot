a4d123425fa87dd1aa16d99fe599240602bdeeaf
Oussama Cherni <oussama.cherni@elyadata.com>
2022-12-23 13:29:45 +0100

    initial commit

---
+++ src/test/java/com/example/schematenancy/SchemaTenancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schematenancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaTenancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
